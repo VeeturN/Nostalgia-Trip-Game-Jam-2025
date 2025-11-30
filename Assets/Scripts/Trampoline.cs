8ed477e12225f020d5579e6936d476d3d8d28081
Kinga Kowalska <kowalska.kinga2004@gmail.com>
2025-11-30 02:08:06 +0100

    add trampoline, fucing grass itd

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class JumpingTrap : MonoBehaviour
 {
     [SerializeField] private float impactSpeed = 10f;
+    private Animator _animator;
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")||collision.gameObject.CompareTag("Player2"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Player2"))
         {
+            _animator.SetBool("isThrowing", true);
             GameObject player = collision.collider.gameObject;
             if (player != null)
             {
@@ -17,5 +23,9 @@ public class JumpingTrap : MonoBehaviour
                 rb2d.AddForce(Vector2.up * impactSpeed, ForceMode2D.Impulse);
             }
         }
+    }
+    public void EndThrowing()
+    {
+        _animator.SetBool("isThrowing", false);
     }
 }
